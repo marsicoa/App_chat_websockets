5746ff9fadd9f66700b89de89eeef9b2996ff632
marsicoa <50459011+marsicoa@users.noreply.github.com>
2022-01-13 13:53:18 -0300

    Add files via upload

--- Desafío No. 6 - Mársico Alfonso/public/javascripts/main.js
+++ Desafío No. 6 - Mársico Alfonso/public/javascripts/main.js
@@ -1,34 +1,61 @@
 let socket = io.connect();
 
-socket.on("mensajes", function (msj) {
-  //console.log(msj);
-  document.getElementById("items").innerHTML = msj
-    .map((msj) => `<span style="color:blue"><b>${msj.email}</b></span> <span style="color:brown">[ ${msj.fyh}]</span>: <span style="font-family:italic; color:green">${msj.mensaje}</span>`)
+const input = document.getElementById("chat-input");
+const email = document.getElementById("email-input");
+const articulo = document.getElementById("articulo-input");
+const precio = document.getElementById("precio-input");
+const miniatura = document.getElementById("miniatura-input");
+
+//--> recibo del server
+socket.on("mensajes", function (msjs) {
+  document.getElementById("msjs").innerHTML = msjs
+    .map(
+      (msj) =>
+        `<span style="color:blue"><b>${msj.email}</b></span> <span style="color:brown">[ ${msj.fyh}]</span>: <span style="font-family:italic; color:green">${msj.mensaje}</span>`
+    )
     .join("<br>");
 });
 
-const input = document.getElementById("chat-input");
-const email = document.getElementById("chat-email")
+socket.on("productos", (arts) => {
+
+  /* fetch('http://localhost:8080/tablita.ejs')
+    .then(function(response) {
+        // When the page is loaded convert it to text
+        return response.text()
+    })
+    .then(function(ejs) {
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(ejs, "text/html");
+        document.getElementById("arts").innerHTML = doc.body.innerHTML
+    })
+    .catch(function(err) {  
+        console.log('Failed to fetch page: ', err);  
+    }); */
+
+  document.getElementById("arts").innerHTML =  arts
+    .map(
+      (art) =>
+        ` <tr><td>${art.title}</td><td>$${art.price}</td><td><img class="medicine"
+          src="https://cdn1.iconfinder.com/data/icons/medical-2-19/512/medical-healthcare-hospital-12-512.png">
+          </td></tr> `
+    )
+    .join("<br>");
+});
+//<--
+
+//--> envuio al server
 document.getElementById("chat-btn").addEventListener("click", () => {
-    const fyh = new Date().toLocaleString()
-    email.value ? socket.emit("mensaje", {msj: input.value, email: email.value, fyh: fyh}) : alert("Debe ingresar su email")
+  const fyh = new Date().toLocaleString();
+  email.value
+    ? socket.emit("mensaje", { msj: input.value, email: email.value, fyh: fyh })
+    : alert("Debe ingresar su email");
 });
 
-/* socket.on("connection", () => {
-  const recuperarTabla = async () => {
-    try {
-      const res = await fetch("http://localhost:8080/partials/tabla.ejs", {
-        method: "GET",
-      });
-      //const data = await res.json()
-      //console.log(data)
-      return res;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  let response = recuperarTabla().then((data) => data);
-  console.log(response);
-  return response;
+document.getElementById("form-btn").addEventListener("click", () => {
+  socket.emit("producto", {
+    title: articulo.value,
+    price: precio.value,
+    thumbnail: miniatura.value,
+  });
 });
- */
+//<--
--- Desafío No. 6 - Mársico Alfonso/server.js
+++ Desafío No. 6 - Mársico Alfonso/server.js
@@ -6,8 +6,45 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const router = require("./routes/productos.js");
+const fetch = require("cross-fetch");
+const fs = require("fs");
 
-const mensajes = [];
+const mensajes  = [];
+const articulos = [];
+
+const respProductos = async () => {
+
+  const listar = async () => {
+      const response = await fetch("http://localhost:8080/txt/productos.txt", {method: "GET"});
+      const res = await response.json()
+      res.forEach(element => {
+        articulos.push(element)
+      });
+      return res
+  }
+
+  let response = await listar().then((res) => {return res})
+  return response
+  
+}
+respProductos()
+
+const respMensajes = async () => {
+
+  const listar = async () => {
+      const response = await fetch("http://localhost:8080/txt/mensajes.txt", {method: "GET"});
+      const res = await response.json()
+      res.forEach(element => {
+        mensajes.push(element)
+      });
+      return res
+  }
+
+  let response = await listar().then((res) => {return res})
+  return response
+  
+}
+respMensajes()
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
@@ -16,27 +53,33 @@ app.set("views", "./views");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-//app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "views")));
 
 io.on("connection", (socket) => {
 
-  /* Eventos conexion/desconexion */  
   console.log(`Conexión establecida - usuario: ${socket.id}`);
   socket.on("disconnect", () => {
     console.log(`Usuario ${socket.id} desconectado`);
   });
 
-  /* Envio los mensajes al cliente que se conectó */
+  //--> envio al cliente
   socket.emit("mensajes", mensajes);
+  socket.emit("productos", articulos);
+  //<--
 
-  /* Escucho los mensajes enviado por el cliente y se los propago a todos */
-  socket.on("mensaje", (data) => {
-    
-    /* escribir un archivo para persistir los mensajes */
-
+  //--> recibo del cliente
+  socket.on("producto", async (data) => {
+    articulos.push(data);
+    await fs.promises.writeFile('./public/txt/productos.txt', JSON.stringify(articulos))
+    io.sockets.emit("productos", articulos);
+  });
+  socket.on("mensaje", async (data) => {
     mensajes.push({ email: data.email, fyh: data.fyh, mensaje: data.msj });
+    await fs.promises.writeFile('./public/txt/mensajes.txt', JSON.stringify(mensajes))
     io.sockets.emit("mensajes", mensajes);
   });
+  //<--
+
 });
 
 app.use("/", router);
